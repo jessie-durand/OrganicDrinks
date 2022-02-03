20c0a49317c22d4e7f55a7e96b33abbfcbe48682
jessie-durand <jessie_durand@hotmail.fr>
2022-02-03 19:39:59 +0100

    ça prend forme

--- src/Form/IngredientType.php
+++ src/Form/IngredientType.php
@@ -5,7 +5,6 @@ namespace App\Form;
 use App\Entity\Drink;
 use App\Entity\Benefit;
 use App\Entity\Ingredient;
-use App\Service\FiltreService;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
@@ -15,13 +14,6 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class IngredientType extends AbstractType
 {
-    public FiltreService $filtreService;
-
-
-    public function __construct(FiltreService $filtreService)
-    {
-        $this->filtreService = $filtreService;
-    }
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
